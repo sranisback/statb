0be459ae3048a3e6c3d30fb04fb56b5ee91084df
Sran_isback <Sran_isback@hotmail.com>
2018-10-04 13:33:58 +0200

    corrections webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ Encore
    // .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
     // .enableVersioning(Encore.isProduction())
+	//.setManifestKeyPrefix('public/build/')
 
     // uncomment to define the assets of the project
 	.addEntry('js/app', './assets/js/app.js')
