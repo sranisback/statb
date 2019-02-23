ffb555e87ae7e4e6c915b8829ce406db98a07d0a
Sran_isback <Sran_isback@hotmail.com>
2019-02-23 08:23:18 +0100

    Maj Bootstrap/yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,7 +11,7 @@ import './jquery.dataTables.min.js'
 import Routing from './router.min.js';
 
 //json route prod
-
+/*
 const routes = {
     "base_url": "http://statbrutedebowl.url.ph/statb/public",
     "routes": {
@@ -269,9 +269,9 @@ const routes = {
     "port": "",
     "scheme": "http"
 }
-
+*/
 //json route dev
-/*
+
 const routes = {
     "base_url": "",
     "routes": {
@@ -427,7 +427,7 @@ const routes = {
             "methods": [],
             "schemes": []
         },
-        "remplayer": {
+        "remPlayer": {
             "tokens": [["variable", "\/", "[^\/]++", "playerId"], ["text", "\/remPlayer"]],
             "defaults": [],
             "requirements": [],
@@ -529,7 +529,7 @@ const routes = {
     "port": "",
     "scheme": "http"
 }
-*/
+
 $(document).ready(function () {
 
     Routing.setRoutingData(routes);
