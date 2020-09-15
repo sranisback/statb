e7d741b312569a63b10fec61c74bbc40b9a75eba
Sran_isback <Sran_isback@hotmail.com>
2020-09-15 11:32:37 +0200

    ajout crud Defis

---
+++ src/Form/admin/DefisType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form\admin;
+
+use App\Entity\Defis;
+use App\Entity\Matches;
+use App\Entity\Teams;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DefisType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('defieRealise')
+            ->add('dateDefi', DateType::class, ['widget'=>'single_text', 'html5' => true])
+            ->add('equipeDefiee', EntityType::class, ['class' => Teams::class, 'choice_label' =>'name'])
+            ->add('matchDefi',EntityType::class,
+                [
+                    'class' => Matches::class,
+                    'choice_label' => function ( Matches $match) {
+                        return $match->getMatchId() . ' - ' .
+                            $match->getTeam1()->getName() . ' vs ' .
+                            $match->getTeam2()->getName();
+                    }
+                ])
+            ->add('equipeOrigine', EntityType::class, ['class' => Teams::class, 'choice_label' =>'name'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Defis::class,
+        ]);
+    }
+}
