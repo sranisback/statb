2e11f61d3c144c7e2f5629ceb30c7b50f21fa1d2
sran_isback <sran_isback@hotmail.com>
2022-11-25 01:35:38 +0100

    maj deprecated

--- src/Entity/Coaches.php
+++ src/Entity/Coaches.php
@@ -6,7 +6,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\OneToMany;
-use Doctrine\ORM\PersistentCollection;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -16,8 +17,11 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @ORM\Table(name="coaches")
  * @ORM\Entity(repositoryClass="App\Repository\CoachesRepository")
  * @method string getUserIdentifier()
+ * @method string hashPassword(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+ * @method bool isPasswordValid(PasswordAuthenticatedUserInterface $user, string $plainPassword)
+ * @method bool needsRehash(PasswordAuthenticatedUserInterface $user)
  */
-class Coaches implements UserInterface
+class Coaches implements UserInterface, UserPasswordHasherInterface, PasswordAuthenticatedUserInterface
 {
     /**
      *
