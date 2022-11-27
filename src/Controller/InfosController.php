59a1b5c895f58fa8ecab2eefc3924dde71cae1a8
sran_isback <sran_isback@hotmail.com>
2022-11-27 01:50:44 +0100

    Infos Controler refacto

--- src/Controller/InfosController.php
+++ src/Controller/InfosController.php
@@ -3,19 +3,28 @@
 namespace App\Controller;
 
 use App\Entity\Infos;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class InfosController extends AbstractController
 {
+
+    private ManagerRegistry $doctrine;
+
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     /**
      * @Route("/infos", name="infos")
      */
     public function infos(): Response
     {
         return $this->render('statbb/infos.html.twig', [
-            'news' => $this->getDoctrine()->getRepository(Infos::class)->cinqDernieresNews(),
+            'news' => $this->doctrine->getRepository(Infos::class)->cinqDernieresNews(),
         ]);
     }
 
@@ -25,7 +34,7 @@ class InfosController extends AbstractController
     public function infosCompletes(): Response
     {
         return $this->render('statbb/infosCompletes.html.twig', [
-            'news' => $this->getDoctrine()->getRepository(Infos::class)->findAll()
+            'news' => $this->doctrine->getRepository(Infos::class)->findAll()
         ]);
     }
 }
